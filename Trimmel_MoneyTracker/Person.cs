373ca6a28acda6221356cf102c2d5539ae43f738
Muhammed Durmaz <durmaz.m02@htlwienwest.at>
2021-01-16 22:02:40 +0100

    konstruktur verändert

--- Trimmel_MoneyTracker/Person.cs
+++ Trimmel_MoneyTracker/Person.cs
@@ -8,7 +8,7 @@ namespace Trimmel_MoneyTracker
 {
     class Person
     {
-        internal string Name { get; set; }
+        internal string Vorname { get; set; }
         internal int Age { get; set; }
         internal int Money { get; set; }
 
@@ -16,7 +16,7 @@ namespace Trimmel_MoneyTracker
 
         public Person(string name, int age, int money)
         {
-            Name = name;
+            Vorname = name;
             Age = age;
             Money = money;
         }
